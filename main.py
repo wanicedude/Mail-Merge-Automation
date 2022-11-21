f216c81e81cf617a13b734beccabae588a73fdd3
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-11-21 09:11:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 #TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
+
+letter = open("../../Documents/GITHUB//Mail Merge Automation/Input/Letters/starting_letter.txt")
+content = letter.read()
+print(content)
+
+#use a for loop to create a list of names in the invited names .txt
+names = []
+with open("../../Documents/GITHUB//Mail Merge Automation/Input/Names/invited_names.txt") as invited_names:
+    name_content = invited_names.readlines()
+        
+    # This will remove the "\n" in each names
+    for x in name_content:
+        names_with = x.replace("\n", "")
+        names.append(names_with)
+print(names)
+
+
+    
 #Replace the [name] placeholder with the actual name.
+
 #Save the letters in the folder "ReadyToSend".
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
